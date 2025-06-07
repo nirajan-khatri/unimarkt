fa927f14a2832d72667fc85c2b5da85ce400ed4d
nirajan-khatri <ykhatri508@gmail.com>
2025-06-07 16:18:15 +0200

    fix: typo in layout file

--- Frontend/unimartk/app/(home)/layout.tsx
+++ Frontend/unimartk/app/(home)/layout.tsx
@@ -1,4 +1,4 @@
-import { Footer } from "@/components/footer";
+import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/navbar";
 import { fetchCategories } from "@/modules/home/api";
 
