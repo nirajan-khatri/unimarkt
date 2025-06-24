cf6fe7a9282c52df01cc4240e87452d30443a252
fd0001550 <inchara-rao.konagolli-shrikanth@informatik.hs-fulda.de>
2025-06-24 22:54:37 +0200

    added current-user api

--- Backend/UniMarkt/uniMarktAuth/urls.py
+++ Backend/UniMarkt/uniMarktAuth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterAPIView, RoleListView, SecurityQuestionChoiceView, PasswordResetAPIView
+from .views import LoginAPIView, RegisterAPIView, RoleListView, SecurityQuestionChoiceView, PasswordResetAPIView,CurrentUserAPIView
 
 urlpatterns = [
     path('login/', LoginAPIView.as_view(), name='login'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path("roles/", RoleListView.as_view(), name="roles"),
     path("security-questions/", SecurityQuestionChoiceView.as_view(), name="security-questions"),
     path('password-reset/', PasswordResetAPIView.as_view(), name='password-reset'),
+    path('current-user/', CurrentUserAPIView.as_view(), name='current-user')
 ]
