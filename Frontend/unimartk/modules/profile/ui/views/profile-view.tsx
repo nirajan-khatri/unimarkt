b3ea680749a099a4144b8d68c7f83e5462524642
nirajan-khatri <ykhatri508@gmail.com>
2025-07-13 23:17:54 +0200

    fix: profile page tabs

--- Frontend/unimartk/modules/profile/ui/views/profile-view.tsx
+++ Frontend/unimartk/modules/profile/ui/views/profile-view.tsx
@@ -435,7 +435,7 @@ const ProfileView = () => {
           <TabsList>
             <TabsTrigger value="products">Products</TabsTrigger>
             <TabsTrigger value="services">Services</TabsTrigger>
-            { (
+            {user?.role !== "user" && (
               <TabsTrigger value="jobs">Jobs</TabsTrigger>
             )}
           </TabsList>
